6ea5752e2bf0d9bdbc878f9b474f3041653e20e7
krishnavamsi1998 <106548869+krishnavamsi1998@users.noreply.github.com>
2022-07-17 11:57:51 +0530

    Create DetectLoopHashMap.java

---
+++ DetectLoopHashMap.java
@@ -0,0 +1,20 @@
+class DetectLoopHashMap {
+    //Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head){      
+        
+        if(head==null)
+           return false;
+           
+        
+        HashSet<Node> set=new HashSet<>();
+        Node temp=head;
+        while(temp!=null)
+        {
+            if(!set.add(temp))
+                return true;
+            temp=temp.next;
+        }
+        return false;
+        
+    }
+}
